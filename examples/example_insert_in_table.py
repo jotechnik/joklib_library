b33933643cfc3aa499f45390f1770d4827c46f0c
J-O. Technik <j-o.technik@web.de>
2020-07-15 15:47:30 +0200

    Delete example_insert_in_table.py

--- examples/example_insert_in_table.py
+++
@@ -1,7 +0,0 @@
-#Copyright 2020 Jan-Ole G. (J-O. Technik)
-#This example puts dada in your table.
-from joklib import database
-
-db = database('yourDB.db')
-db.insertintable('yourTable', 'yourdata')
-db.close()
