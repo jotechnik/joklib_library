ed764e42c03b2296d0315230cdcbd4e853e60fd5
J-O. Technik <jan-ole.giebel@jo-technik.com>
2021-01-25 13:58:16 +0100

    Delete joklib_vers0.1 directory

--- joklib_vers0.1/examples/example_klimabox_read_durchschnitt.py
+++
@@ -1,7 +0,0 @@
-#Copyright 2020 Jan-Ole G. (J-O. Technik)
-#In this example data is read from the database and print out on the screen.
-from joklib import klimbox
-
-box = klimbox('http://yourHost', 80)
-box.boxin('yourRoomTable', 'wifi')
-print(box.data)
--- joklib_vers0.1/examples/example_stdio.py
+++
@@ -1,8 +0,0 @@
-#Copyright 2020 Jan-Ole G. (J-O. Technik)
-#This example demonstrates the stdio class of the joklib library.
-from joklib import stdio
-
-io = stdio()
-io.sage("Hallo Welt!!!")
-ein = io.frage("Wie geht es dir???")
-io.sage("Dir geht es: "+ein)
